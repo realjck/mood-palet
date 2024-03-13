8acf55239239d565a643eb7a3fb4f955eba1fb1b
jck <jckleinbourg@gmail.com>
2024-03-14 00:24:18 +0100

    ✨ Add login route screen with layout template and style

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-from flask import Flask, url_for
+from flask import Flask, render_template, request, session, redirect, url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return f"<h1>Hello, World!</h1><p>Route: {url_for('hello_world')}</p>"
+app.secret_key = b'a449a3e361391583a64fc758349592acebf6a5e801902686704c6a179e35c64b'
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
+
+@app.get('/login')
+def login_get():
+    return render_template('login.html')
+
+@app.post('/login')
+def login_post():
+    username = request.form.get('username')
+    return render_template('manager.html', name=username)
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
