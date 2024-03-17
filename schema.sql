ba55846cc24ae96706cda733c905aafa36fb3d28
jck <jckleinbourg@gmail.com>
2024-03-17 21:25:49 +0100

    🗃️ Add table 'palet' to database schema

--- schema.sql
+++ schema.sql
@@ -3,3 +3,13 @@ CREATE TABLE user (
     name TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
+
+CREATE TABLE palet (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    date TIMESTAMP NOT NULL,
+    title TEXT NOT NULL,
+    colors TEXT NOT NULL,
+    url TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id)
+);
